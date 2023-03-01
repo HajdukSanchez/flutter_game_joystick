1557af0cc5a9e822b2e5a34bd3539ecf48c96aa4
HajdukSanchez <hajduksanchez.dev@gmail.com>
2023-03-01 10:03:34 -0500

    🎉 [BEGIN] New project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flame/flame.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+
+  runApp(Container());
+}
